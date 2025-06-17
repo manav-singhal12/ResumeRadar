cc323b7d41d59a388c096f8e49d9d481a5fac0a8
manav-singhal12 <manavsinghal165@gmail.com>
2025-06-18 02:01:59 +0530

    first commit

--- server/src/index.js
+++ server/src/index.js
@@ -10,12 +10,16 @@ const app = express();
 
 app.use(
   cors({
-    origin: ServerConfig.Frontend_URL || 'https://resume-radar-five.vercel.app/',
+    origin: [
+      'http://localhost:5173',
+      'https://resume-radar-five.vercel.app'
+    ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
   })
 );
+
 console.log(ServerConfig)
 
 app.use(express.json());
